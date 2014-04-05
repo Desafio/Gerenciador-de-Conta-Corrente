b60413a282e402b6b989e3bcdb261b88cb80f2c4
doffrede <david.offrede@infoglobo.com.br>
2014-04-05 15:00:19 -0300

    colocando click no botao para chamar a home

--- GerenciadorDeConta/src/com/example/gerenciadordeconta/fragments/LoginFragment.java
+++ GerenciadorDeConta/src/com/example/gerenciadordeconta/fragments/LoginFragment.java
@@ -1,11 +1,14 @@
 package com.example.gerenciadordeconta.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.desafio.gerenciadordeconta.activitys.HomeActivity;
 import com.example.gerenciadordeconta.R;
 
 public class LoginFragment extends Fragment {
@@ -18,6 +21,16 @@ public class LoginFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_login,
 				container, false);
-		return rootView;
+		
+		
+		final Button button = (Button) rootView.findViewById(R.id.button_entrar);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Intent intent = new Intent(getActivity(), HomeActivity.class);
+            	getActivity().startActivity(intent);
+            }
+        });
+        
+        return rootView;
 	}
 }
