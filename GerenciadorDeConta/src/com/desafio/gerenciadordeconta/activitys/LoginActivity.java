6903918d2361216e3bacf061383a07e16a11aa36
David Offrede <david.offrede@infoglobo.com>
2014-04-09 15:35:44 -0300

    Usando SP para adc as contas

--- GerenciadorDeConta/src/com/desafio/gerenciadordeconta/activitys/LoginActivity.java
+++ GerenciadorDeConta/src/com/desafio/gerenciadordeconta/activitys/LoginActivity.java
@@ -1,16 +1,18 @@
 package com.desafio.gerenciadordeconta.activitys;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.activeandroid.query.Delete;
 import com.desafio.gerenciadordeconta.fragments.LoginFragment;
 import com.desafio.gerenciadordeconta.models.ContaCorrente;
 import com.example.gerenciadordeconta.R;
 
 public class LoginActivity extends ActionBarActivity {
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +28,35 @@ public class LoginActivity extends ActionBarActivity {
 	}
 
 	private void criarContaCorrente() {
-		new Delete().from(ContaCorrente.class).execute();
-
-		ContaCorrente contaCorrente1 = new ContaCorrente();
-		contaCorrente1.setConta("11111");
-		contaCorrente1.setSenha("1111");
-		contaCorrente1.setVIP(true);
-		contaCorrente1.save();
 		
-		ContaCorrente contaCorrente2 = new ContaCorrente();
-		contaCorrente2.setConta("22222");
-		contaCorrente2.setSenha("2222");
-		contaCorrente2.setVIP(false);
-		contaCorrente2.save();
+		SharedPreferences sharedPreferences = getSharedPreferences("Preference", MODE_PRIVATE);
+		if (sharedPreferences.getBoolean(
+				"keyConta", true)) {
+			ContaCorrente contaCorrente1 = new ContaCorrente();
+			contaCorrente1.setConta("11111");
+			contaCorrente1.setSenha("1111");
+			contaCorrente1.setVIP(true);
+			contaCorrente1.setSaldo(1000.0F);
+			contaCorrente1.save();
+			
+			ContaCorrente contaCorrente2 = new ContaCorrente();
+			contaCorrente2.setConta("22222");
+			contaCorrente2.setSenha("2222");
+			contaCorrente2.setVIP(false);
+			contaCorrente2.setSaldo(500.0F);
+			contaCorrente2.save();
+			
+			ContaCorrente contaCorrente3 = new ContaCorrente();
+			contaCorrente3.setConta("33333");
+			contaCorrente3.setSenha("3333");
+			contaCorrente3.setVIP(true);
+			contaCorrente1.setSaldo(100.0F);
+			contaCorrente3.save();
+		}
+		
+		SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("keyConta", false);
+        editor.commit();
 	}
 
 	@Override
--- GerenciadorDeConta/src/com/desafio/gerenciadordeconta/models/ContaCorrente.java
+++ GerenciadorDeConta/src/com/desafio/gerenciadordeconta/models/ContaCorrente.java
@@ -1,12 +1,10 @@
 package com.desafio.gerenciadordeconta.models;
 
 import java.io.Serializable;
-import java.util.List;
 
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
-import com.activeandroid.query.Select;
 
 @Table(name = "correntista")
 public class ContaCorrente extends Model implements Serializable {
@@ -24,6 +22,9 @@ public class ContaCorrente extends Model implements Serializable {
 
 	@Column(name = "VIP")
 	private Boolean VIP;
+	
+	@Column(name = "saldo")
+	private float saldo;
 
 	public ContaCorrente() {
 	}
@@ -52,15 +53,11 @@ public class ContaCorrente extends Model implements Serializable {
 		return VIP;
 	}
 	
-	public String getSaldo() {
-		List<Transferencia> extratoList = new Select().from(Transferencia.class)
-					.where("conta = ?", this.getConta()).execute();
-		float saldo = 0.0F;
-		
-		for(Transferencia transferencia : extratoList) {
-			saldo = saldo + transferencia.getValor();
-		}
-		
-		return Float.toString(saldo);
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	
+	public float getSaldo() {
+		return saldo;
 	}
 }
