c2dfffb074b1e656a1adbbf411b30a268c6f868a
doffrede <david.offrede@infoglobo.com.br>
2014-04-09 00:11:30 -0300

    Colocando erro caso a conta ou a senha estiverem erradas

--- GerenciadorDeConta/src/com/desafio/gerenciadordeconta/fragments/LoginFragment.java
+++ GerenciadorDeConta/src/com/desafio/gerenciadordeconta/fragments/LoginFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.activeandroid.query.Select;
 import com.desafio.gerenciadordeconta.activitys.HomeActivity;
@@ -24,29 +25,38 @@ public class LoginFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_login,
-				container, false);
-		
-		final EditText loginConta = (EditText) rootView.findViewById(R.id.contaCorrente);
-		final EditText loginSenha = (EditText) rootView.findViewById(R.id.senha);
-		
-		final Button button = (Button) rootView.findViewById(R.id.button_entrar);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	String stringConta = loginConta.getText().toString();
-            	String stringSenha = loginSenha.getText().toString();
-            	
-            	List<ContaCorrente> list = new Select().from(ContaCorrente.class).where("conta = ? AND senha = ?", stringConta, stringSenha)
-        				.execute();
-            	
-            	if(list.size() == 1){
-            		Intent intent = new Intent(getActivity(), HomeActivity.class);
-            		intent.putExtra("conta", list.get(0).getConta());
-                	getActivity().startActivity(intent);
-            	}
-            }
-        });
-        
-        return rootView;
+		View rootView = inflater.inflate(R.layout.fragment_login, container,
+				false);
+
+		final EditText loginConta = (EditText) rootView
+				.findViewById(R.id.contaCorrente);
+		final EditText loginSenha = (EditText) rootView
+				.findViewById(R.id.senha);
+
+		final Button button = (Button) rootView
+				.findViewById(R.id.button_entrar);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				String stringConta = loginConta.getText().toString();
+				String stringSenha = loginSenha.getText().toString();
+
+				List<ContaCorrente> list = new Select()
+						.from(ContaCorrente.class)
+						.where("conta = ? AND senha = ?", stringConta,
+								stringSenha).execute();
+
+				if (list == null || list.size() != 1) {
+					Toast.makeText(getActivity(), "Conta ou Senha inválida.", Toast.LENGTH_LONG).show();
+					return;
+				}
+				
+				Intent intent = new Intent(getActivity(), HomeActivity.class);
+				intent.putExtra("conta", list.get(0).getConta());
+				getActivity().startActivity(intent);
+
+			}
+		});
+
+		return rootView;
 	}
 }
