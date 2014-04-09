e28f212ada4f96003bac007ab712127218cd42a8
doffrede <david.offrede@infoglobo.com.br>
2014-04-09 03:52:40 -0300

    Construindo funcionalidade Deposito

--- GerenciadorDeConta/src/com/desafio/gerenciadordeconta/fragments/DepositoFragment.java
+++ GerenciadorDeConta/src/com/desafio/gerenciadordeconta/fragments/DepositoFragment.java
@@ -1,13 +1,18 @@
 package com.desafio.gerenciadordeconta.fragments;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.desafio.gerenciadordeconta.models.ContaCorrente;
+import com.desafio.gerenciadordeconta.models.Transferencia;
 import com.example.gerenciadordeconta.R;
 
 public class DepositoFragment extends Fragment {
@@ -25,8 +30,25 @@ public class DepositoFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_deposito, container, false);
         contaCorrente = (ContaCorrente) getActivity().getIntent().getSerializableExtra("conta");
         
-        TextView text = (TextView) rootView.findViewById(R.id.section_label);
-        text.setText(contaCorrente.getConta() + " - Depósito");
+        final EditText loginSenha = (EditText) rootView
+				.findViewById(R.id.deposito_valor);
+
+		final Button button = (Button) rootView
+				.findViewById(R.id.button_depositar);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				String valor = loginSenha.getText().toString();
+				Calendar currentDate = Calendar.getInstance();
+				Transferencia transferencia = new Transferencia();
+				transferencia.setConta(contaCorrente.getConta());
+				transferencia.setData(currentDate.getTime());
+				transferencia.setDescricao("Deposito");
+				transferencia.setValor(Float.valueOf(valor));
+				transferencia.save();
+				
+				Toast.makeText(getActivity(), "Deposito realizado com sucesso.", Toast.LENGTH_LONG).show();
+			}
+		});
         return rootView;
 	}
 }
